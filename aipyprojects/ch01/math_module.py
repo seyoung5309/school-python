43e962b1d8a666c3423ac350adf1886941179f00
Seyoung Oh <s2525@e-mirim.hs.kr>
2026-08-25 12:10:13 +0900

    feat: 0825 수업 끝

---
+++ aipyprojects/ch01/math_module.py
@@ -0,0 +1,10 @@
+import math
+
+print('pi 상수', math.pi)
+print('자연 상수 e', math.e)
+print('올림 - 3.5의 올림 : ', math.ceil(3.5))
+print('내림 - 3.5의 내림 : ', math.floor(3.5))
+print('팩토리얼 - 5! : ', math.factorial(5))
+print('최대공약수 - 28과 70 : ', math.gcd(28, 70))
+print('최소공배수 - 28과 70 : ', math.lcm(28, 70))
+print('거듭제곱 - 2의 10제곱 : ', math.pow(2, 10))
